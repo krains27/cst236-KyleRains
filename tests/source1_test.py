e5040390ea4a4fc4b639a5710f69fe06ccb160c3
krains27 <krains27@gmail.com>
2015-04-03 21:17:12 -0700

    Fixed test on master to stop build failure.

--- tests/source1_test.py
+++ tests/source1_test.py
@@ -12,4 +12,4 @@ class TestGetTriangleType(TestCase):
 
     def test_get_triangle_scalene_all_int(self):
         result = get_triangle_type(1, 2, 3)
-        self.assertEqual(result, 'equilateral')
+        self.assertEqual(result, 'scalene')
